3013999344d7369cc43164fcf4e50a423de58a47
Chris Dore <oesolutions@gmail.com>
2014-05-09 17:10:20 -0600

    Initial version that attempts to use the Gradle Tooling API.

--- src/main/java/ToolingTask.java
+++ src/main/java/ToolingTask.java
@@ -1,39 +1,46 @@
 
-
-//@GrabResolver(name='gradle', root='http://repo.gradle.org/gradle/libs-releases-local')
-//@Grab(group='org.gradle', module='gradle-tooling-api', version='1.11')
+import java.io.File;
+import java.lang.Exception;
 
 import hudson.FilePath;
 import hudson.FilePath.FileCallable;
 import hudson.remoting.VirtualChannel;
-import java.io.File;
 
-//import org.gradle.tooling.*;
-//import org.gradle.tooling.model.*
+import org.gradle.tooling.*;
+import org.gradle.tooling.model.*;
 
-public class TestWrapper {
 
-    public String stringFunc(FilePath path)
+public class ToolingTask {
+
+    public String stringFunc(FilePath path) throws Exception
     {
-        return path.act(new ToolingTask())
+        String ret = null;
+        //try {
+            ret = path.act(new ToolingTaskHelper());
+        //}
+        //catch (Exception e) {
+        //    ret = e.toString() + "\n\n" + e.getMessage();
+        //}
+
+        return ret;
     }
 
-    private static final class ToolingTask implements FileCallable<String> {
+    private static final class ToolingTaskHelper implements FileCallable<String> {
         private static final long serialVersionUID = 1;
 
         @Override public String invoke(File f, VirtualChannel channel) {
             String ret = "nothing";
-            //GradleConnector connector = GradleConnector.newConnector()
-            //connector.forProjectDirectory(f)
-            //ProjectConnection connection = connector.connect()
-            //try {
+            GradleConnector connector = GradleConnector.newConnector();
+            connector.forProjectDirectory(f);
+            ProjectConnection connection = connector.connect();
+            try {
                 // Load the model for the project
-            //    GradleProject project = connection.getModel(GradleProject.class)
-            //    ret = project.getName()
-            //} finally {
+                GradleProject project = connection.getModel(GradleProject.class);
+                ret = project.getName();
+            } finally {
                 // Clean up
-            //    connection.close();
-            //}
+                connection.close();
+            }
 
             //ret = a.isDirectory() ? "yes" : "no"
             //String[] stuff = f.list();
